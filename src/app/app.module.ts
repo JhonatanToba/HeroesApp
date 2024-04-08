01d08c54c3bc17e1a7bf3656c0ddf8ba44b397c3
Jhonatan Leonardo Toba Silva <leonardotoba1998@gmail.com>
2024-04-08 13:58:01 -0500

    Se añade consumo de JSON SERVER para hacer de back junto con modificación de diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
     SharedModule,
   ],
